5cee986ffdc038b261cf703a232ecbcd35798b49
Emerson Santana <emersonsouzasantana@outlook.com>
2026-06-15 12:24:52 -0300

    Correcao: mudando o nome do metodo para movimentoPossivel

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -40,8 +40,8 @@
             }
             return false;
         }
-
-        public bool podeMoverPara(Posicao pos)
+        
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
